4d9ebe761378867cdefaf7a661ff76cb34c2b95c
hirohito doi <00900065@hasekou.local>
2020-09-07 19:03:25 +0900

    不要な記述の削除

--- src/store/imageSlice.jsx
+++ src/store/imageSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const fetchImageAsync = () => {
   return async (dispatch, getState) => {
